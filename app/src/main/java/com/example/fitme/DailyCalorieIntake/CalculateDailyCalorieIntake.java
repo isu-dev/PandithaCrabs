44f64af3fa7e5d8c40ed7b0cde43ccf2fef94988
Asel Thalawaththa <aselb046@gmail.com>
2020-09-10 18:03:03 +0530

    Calorie calculator first commit

---
+++ app/src/main/java/com/example/fitme/DailyCalorieIntake/CalculateDailyCalorieIntake.java
@@ -0,0 +1,16 @@
+package com.example.fitme.DailyCalorieIntake;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fitme.R;
+
+public class CalculateDailyCalorieIntake extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculate_daily_calorie_intake);
+    }
+}
