1618200f942652f479d84932c2d2cd599608dd45
Asel Thalawaththa <aselb046@gmail.com>
2020-10-02 13:16:21 +0530

    Main app screen added

---
+++ app/src/main/java/com/example/fitme/AppCommon/MainAppScreen.java
@@ -0,0 +1,16 @@
+package com.example.fitme.AppCommon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fitme.R;
+
+public class MainAppScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_app_screen);
+    }
+}
